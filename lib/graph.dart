7dd143d945f69e540fd0e1f9cfd5378acaccbf24
Sebastian Weddmark Olsson <visnae@gmail.com>
2021-09-05 22:35:14 +0200

    Add FloatingActionButtons

--- lib/graph.dart
+++ lib/graph.dart
@@ -11,8 +11,6 @@ class GraphWidget extends StatefulWidget {
 }
 
 class _GraphWidgetState extends State<GraphWidget> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-
   List<Map<String, Object>> _data1 = [{ 'name': 'Please wait', 'value': 0 }];
 
   getData1() async {
@@ -53,21 +51,11 @@ class _GraphWidgetState extends State<GraphWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(
-        title: Text('CoinTrack'),
-      ),
-      backgroundColor: Colors.black,
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Container(
-                child: Echarts(
-                  captureAllGestures: true,
-                  theme: 'dark',
-                  option: '''
+    return Container(
+      child: Echarts(
+        captureAllGestures: true,
+        theme: 'dark',
+        option: '''
                     {
                       tooltip: {
                           trigger: 'axis',
@@ -94,7 +82,7 @@ class _GraphWidgetState extends State<GraphWidget> {
                       },
                       yAxis: {
                           type: 'value',
-                          boundaryGap: [0, '100%']
+                          boundaryGap: [0, '10%']
                       },
                       dataZoom: [{
                           type: 'inside',
@@ -104,9 +92,9 @@ class _GraphWidgetState extends State<GraphWidget> {
                           start: 0,
                           end: 100,
                           handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
-                          handleSize: '80%',
+                          handleSize: '50%',
                           handleStyle: {
-                              color: '#fff',
+                              color: '#22ffff',
                               shadowBlur: 3,
                               shadowColor: 'rgba(0, 0, 0, 0.6)',
                               shadowOffsetX: 2,
@@ -136,14 +124,9 @@ class _GraphWidgetState extends State<GraphWidget> {
                       ]
                     }
                   ''',
-                ),
-                width: 300,
-                height: 250,
-              )
-            ],
-          ),
-        ),
       ),
+      width: 300,
+      height: 250,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,61 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'graph.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: GraphWidget(),
+      home: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text('CoinTrack'),
+        ),
+        backgroundColor: Colors.black,
+        body: HomeChooserWidget(),
+        floatingActionButton: SpeedDial(
+          backgroundColor: Colors.red,
+          icon: Icons.add,
+          activeIcon: Icons.close,
+          renderOverlay: false,
+          children: [
+            SpeedDialChild(
+              child: Icon(Icons.app_registration_rounded),
+              label: 'Asset',
+              backgroundColor: Colors.amberAccent,
+              onTap: () {
+
+              },
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
+
+class HomeChooserWidget extends StatefulWidget {
+  @override
+  _HomeChooserState createState() => new _HomeChooserState();
+}
+
+class _HomeChooserState extends State<HomeChooserWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
+          children: <Widget>[GraphWidget()]
+        )
+      )
     );
   }
 }
